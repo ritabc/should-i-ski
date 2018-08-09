d3e5aa9f1945e85f8c785b4c035861e10ed8e3db
Rita Bennett-Chew and scott fraser <rita@>
2018-08-09 10:43:38 -0700

    Format icons, temperatures, and windspeeds

--- src/js/main.js
+++ src/js/main.js
@@ -20,14 +20,26 @@ $(document).ready(function() {
       $('#output').text(`${body.data.nearest_area[0].areaName[0].value}`)
       $('#temperature').text(`${body.data.weather[0].bottom[0].maxtempC}c`)
       $('#chanceOfSnow').text(`${body.data.weather[0].chanceofsnow}%`)
-
       let calc = new Algorithm()
       if (calc.shouldWeSki(body) == true){
-        console.log(city);
         $('#shouldWeSki').text("GET YOUR BOOTS ON!")
       } else {
           $('#shouldWeSki').text("Nope, TIME TO DRINK!")
       }
+
+      let dayOneLow = body.data.weather[0].mid[0].mintempC
+      let dayOneHigh = body.data.weather[0].mid[0].maxtempC
+      $('#day-one-temps').text(`${dayOneLow}C/${dayOneHigh}C`)
+
+      let windspeed = body.data.weather[0].hourly[0].mid[0].windspeedKmph
+      $('#day-one-days').text(`${windspeed}kmph`);
+
+      let iconAM = body.data.weather["0"].hourly[2].bottom["0"].weatherIconUrl["0"].value
+      $('#day-one-pic').html(`<img src=${iconAM} alt="amPic">`)
+
+      let iconPM = body.data.weather["0"].hourly[7].bottom["0"].weatherIconUrl["0"].value
+      $('#night-one-pic').html(`<img src=${iconPM} alt="pmPic">`)
+
 
     })
   })
